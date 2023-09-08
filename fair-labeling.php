3b8b3592aa4b2c3a8de26d6b0a3f236014e08654
Kasiri <135030892+Kasiri-git@users.noreply.github.com>
2023-09-08 17:29:40 +0900

    Add files via upload

--- fair-labeling.php
+++ fair-labeling.php
@@ -28,6 +28,25 @@ function fair_labeling_settings_page() {
             <?php
             settings_fields('fair-labeling-settings-group');
             do_settings_sections('fair-labeling-settings');
+            
+            // 表示オプションを追加
+            $show_on_posts = get_option('fair_labeling_show_on_posts');
+            $show_on_pages = get_option('fair_labeling_show_on_pages');
+            ?>
+            
+            <h3>Show Custom Text On:</h3>
+            
+            <label>
+                <input type="checkbox" name="fair_labeling_show_on_posts" value="1" <?php checked(1, $show_on_posts); ?> />
+                Posts
+            </label><br />
+            
+            <label>
+                <input type="checkbox" name="fair_labeling_show_on_pages" value="1" <?php checked(1, $show_on_pages); ?> />
+                Pages
+            </label><br />
+            
+            <?php
             submit_button();
             ?>
         </form>
@@ -38,11 +57,15 @@ function fair_labeling_settings_page() {
 // 設定項目を登録
 function fair_labeling_register_settings() {
     register_setting('fair-labeling-settings-group', 'fair_labeling_custom_text');
+    
+    // 新しい設定項目を追加
+    register_setting('fair-labeling-settings-group', 'fair_labeling_show_on_posts', 'intval');
+    register_setting('fair-labeling-settings-group', 'fair_labeling_show_on_pages', 'intval');
 }
 
 add_action('admin_init', 'fair_labeling_register_settings');
 
-// 設定メニューページに設定項目を表示
+// 設定項目を表示
 function fair_labeling_settings_field() {
     $custom_text = get_option('fair_labeling_custom_text');
     ?>
@@ -63,14 +86,26 @@ add_action('admin_init', 'fair_labeling_add_settings');
 
 // アイキャッチの下にカスタムテキストを追加
 function fair_labeling_add_custom_text($content) {
-    if (is_single() && has_post_thumbnail()) {
-        $custom_text = get_option('fair_labeling_custom_text'); // ユーザーが入力したカスタムテキストを取得
-        $custom_text_html = '<div class="fair-labeling-custom-text small-text">' . esc_html($custom_text) . '</div>';
+    if (has_post_thumbnail()) {
+        $show_on_posts = get_option('fair_labeling_show_on_posts');
+        $show_on_pages = get_option('fair_labeling_show_on_pages');
         
-        $content = $custom_text_html . $content; // アイキャッチの下にカスタムテキストを追加
+        // 投稿ページまたは固定ページにカスタムテキストを表示
+        if ((is_single() && $show_on_posts) || (is_page() && $show_on_pages)) {
+            $custom_text = get_option('fair_labeling_custom_text'); // ユーザーが入力したカスタムテキストを取得
+            $custom_text_html = '<div class="fair-labeling-custom-text small-text">' . esc_html($custom_text) . '</div>';
+            
+            $content = $custom_text_html . $content; // アイキャッチの下にカスタムテキストを追加
+        }
     }
     return $content;
 }
 
 add_filter('the_content', 'fair_labeling_add_custom_text');
 
+// スタイルシートを読み込む
+function fair_labeling_enqueue_styles() {
+    wp_enqueue_style('fair-labeling-styles', plugin_dir_url(__FILE__) . 'css/fair-labeling-style.css');
+}
+
+add_action('wp_enqueue_scripts', 'fair_labeling_enqueue_styles');
